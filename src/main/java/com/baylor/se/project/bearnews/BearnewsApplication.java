88dca617112829c4b35984d9759682909c558e77
Arju <Muhammad_Arju1@baylor.edu>
2022-09-20 18:28:56 -0500

    First commit

---
+++ src/main/java/com/baylor/se/project/bearnews/BearnewsApplication.java
@@ -0,0 +1,13 @@
+package com.baylor.se.project.bearnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearnewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BearnewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baylor/se/project/bearnews/BearnewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baylor.se.project.bearnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BearnewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
