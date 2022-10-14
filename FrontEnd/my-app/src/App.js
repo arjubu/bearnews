a3c225b6377f9d937c251ab0e78a0880e880cfc4
miao tingshuo <miaotingshuo@gmail.com>
2022-10-14 00:51:03 -0500

    LoginPage Build

--- FrontEnd/my-app/src/App.js
+++ FrontEnd/my-app/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./login";
+// import Main from "./main";
+import React from "react";
+// import Manager from "./manager";
+import "./App.css";
 
-function App() {
+function App(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+      <Route exact path='/' element={< Login />}></Route>
+      {/* <Route exact path='/employee/:id' element={< Main />}></Route>
+      <Route exact path='/manager/:id' element={< Manager />}></Route> */}
+      </Routes>
+      </BrowserRouter>
+      
+      </div>
+    );
 }
 
 export default App;
