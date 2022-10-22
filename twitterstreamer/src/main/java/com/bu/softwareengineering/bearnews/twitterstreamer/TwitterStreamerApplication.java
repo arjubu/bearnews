f800adcbf9fcfc6f7137a715cfe048b26f7b9b10
Ashfakur Rahman Arju <ashfak@Ashfakurs-MacBook-Pro.local>
2022-10-22 17:53:43 -0500

    twitter streamer app added

---
+++ twitterstreamer/src/main/java/com/bu/softwareengineering/bearnews/twitterstreamer/TwitterStreamerApplication.java
@@ -0,0 +1,13 @@
+package com.bu.softwareengineering.bearnews.twitterstreamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterStreamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterStreamerApplication.class, args);
+	}
+
+}
---
+++ twitterstreamer/src/test/java/com/bu/softwareengineering/bearnews/twitterstreamer/TwitterStreamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bu.softwareengineering.bearnews.twitterstreamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterStreamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
