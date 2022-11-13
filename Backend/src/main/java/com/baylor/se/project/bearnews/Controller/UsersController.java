177bcc7d7de744ae26b27146a2a25a675bf0fd07
rahman-mush1311 <tuktuki1311@yahoo.com>
2022-11-13 14:10:45 -0600

    find all users added

--- Backend/src/main/java/com/baylor/se/project/bearnews/Controller/UsersController.java
+++ Backend/src/main/java/com/baylor/se/project/bearnews/Controller/UsersController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class UsersController {
 
@@ -14,10 +16,16 @@ public class UsersController {
     UsersService usersService;
 
 
-    @RequestMapping(path="/applyForApplication", method = RequestMethod.POST)
+    @RequestMapping(path="/applyForAccount", method = RequestMethod.POST)
     @ResponseBody
     public ResponseEntity<?> registerForAccount(@RequestBody Users users) {
         String responseReturned = usersService.RegisterUserToSystem(users);
         return new ResponseEntity<>(responseReturned,HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/findAllUsers", method = RequestMethod.GET)
+    public ResponseEntity<?> getAllUsers(){
+        List<Users> foundApplicants = usersService.listAllUser();
+        return new ResponseEntity<>(foundApplicants,HttpStatus.OK);
     }
 }
