412ff1f1dd8e2f3d36925f88552273dfb2b9e5bd
Sadia Nasrin Tisha <sadiatisha@Sadias-MacBook-Air.local>
2022-11-14 15:49:23 -0600

    dependency added with notification.

--- Backend/src/main/java/com/baylor/se/project/bearnews/Controller/NotificationController.java
+++ Backend/src/main/java/com/baylor/se/project/bearnews/Controller/NotificationController.java
@@ -1,7 +1,12 @@
 package com.baylor.se.project.bearnews.Controller;
 
+import com.baylor.se.project.bearnews.Model.Event;
 import com.baylor.se.project.bearnews.Service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +14,10 @@ public class NotificationController {
 
     @Autowired
     NotificationService notificationService;
+
+
+    @RequestMapping(value = "/allnotification", method = RequestMethod.GET)
+    public ResponseEntity<Event> getNotifications() {
+        return new ResponseEntity(notificationService.getAllNotification(), HttpStatus.OK);
+    }
 }
