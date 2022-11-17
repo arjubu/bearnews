d8bd60571b76c1c75aaae7aef944dd0d18135f32
rahman-mush1311 <tuktuki1311@yahoo.com>
2022-11-17 15:29:00 -0600

    create article and fetch all article service defined

--- Backend/src/main/java/com/baylor/se/project/bearnews/Controller/ArticleController.java
+++ Backend/src/main/java/com/baylor/se/project/bearnews/Controller/ArticleController.java
@@ -1,4 +1,8 @@
 package com.baylor.se.project.bearnews.Controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ArticleController {
+
 }
--- Backend/src/main/java/com/baylor/se/project/bearnews/Service/ArticleService.java
+++ Backend/src/main/java/com/baylor/se/project/bearnews/Service/ArticleService.java
@@ -1,4 +1,28 @@
 package com.baylor.se.project.bearnews.Service;
 
+import com.baylor.se.project.bearnews.Models.Article;
+import com.baylor.se.project.bearnews.Repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ArticleService {
+
+    @Autowired
+    ArticleRepository articleRepository;
+
+    public void createArticle(Article article){
+        if(article.getContent()=="")
+            return;
+        if(article.getTitle()=="")
+            return;
+        else
+            articleRepository.save(article);
+    }
+
+    public List<Article> fetchAllarticles(){
+        return articleRepository.findAll();
+    }
 }
