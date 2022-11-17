3b952acef25d8d4030bb15ada2fd388c7fc44a27
rahman-mush1311 <tuktuki1311@yahoo.com>
2022-11-17 17:45:19 -0600

    create comment service added

--- Backend/src/main/java/com/baylor/se/project/bearnews/Controller/CommentController.java
+++ Backend/src/main/java/com/baylor/se/project/bearnews/Controller/CommentController.java
@@ -1,4 +1,9 @@
 package com.baylor.se.project.bearnews.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CommentController {
+
 }
--- Backend/src/main/java/com/baylor/se/project/bearnews/Service/CommentService.java
+++ Backend/src/main/java/com/baylor/se/project/bearnews/Service/CommentService.java
@@ -1,4 +1,20 @@
 package com.baylor.se.project.bearnews.Service;
 
+import com.baylor.se.project.bearnews.Models.Comment;
+import com.baylor.se.project.bearnews.Repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CommentService {
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    public String createComment(Comment comment){
+        if(comment.getText()=="")
+            return "comment body couldn't be empty";
+        commentRepository.save(comment);
+        return String.valueOf(comment.getId());
+    }
 }
