dbcfd9b201f7a667b1434b5c862569636974d938
rahman-mush1311 <tuktuki1311@yahoo.com>
2022-11-17 15:24:42 -0600

    article model created without relationship

--- Backend/src/main/java/com/baylor/se/project/bearnews/Models/Article.java
+++ Backend/src/main/java/com/baylor/se/project/bearnews/Models/Article.java
@@ -1,5 +1,38 @@
 package com.baylor.se.project.bearnews.Models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Article {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    @Column
+    private String title;
+    @Column
+    private String content;
+    //change it to relationship later
+    @Column
+    private long tagId;
+    //change it to relationship later
+    @Column
+    private long userId;
+    @CreationTimestamp
+    private LocalDateTime createdDateTime;
+
+
+
 }
