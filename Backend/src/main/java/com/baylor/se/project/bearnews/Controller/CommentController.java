0853d61d8011e336a2de64eb9c2e703a45e84af0
rahman-mush1311 <tuktuki1311@yahoo.com>
2022-11-17 17:51:00 -0600

    comment create api added

--- Backend/src/main/java/com/baylor/se/project/bearnews/Controller/CommentController.java
+++ Backend/src/main/java/com/baylor/se/project/bearnews/Controller/CommentController.java
@@ -1,9 +1,31 @@
 package com.baylor.se.project.bearnews.Controller;
 
+import com.baylor.se.project.bearnews.Models.Comment;
+import com.baylor.se.project.bearnews.Models.Tag;
+import com.baylor.se.project.bearnews.Service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CommentController {
 
+    @Autowired
+    CommentService commentService;
+
+    @RequestMapping(value = "/insertComment", method = RequestMethod.POST)
+    public ResponseEntity<?> insertComment(@RequestBody Comment commentSent) {
+      String responseReturned =commentService.createComment(commentSent);
+        int intValue;
+        try {
+            intValue = Integer.parseInt(responseReturned);
+            return new ResponseEntity<>(responseReturned,HttpStatus.OK);
+        } catch (NumberFormatException e) {
+            return new ResponseEntity<>(responseReturned,HttpStatus.BAD_REQUEST);
+        }
+    }
 }
