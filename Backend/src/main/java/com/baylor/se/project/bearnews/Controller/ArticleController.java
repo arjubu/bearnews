a7f46248b0f96e9865bbda605bba2b7fa5db86fc
rahman-mush1311 <tuktuki1311@yahoo.com>
2022-11-17 15:32:53 -0600

    create article api added

--- Backend/src/main/java/com/baylor/se/project/bearnews/Controller/ArticleController.java
+++ Backend/src/main/java/com/baylor/se/project/bearnews/Controller/ArticleController.java
@@ -1,8 +1,24 @@
 package com.baylor.se.project.bearnews.Controller;
 
+import com.baylor.se.project.bearnews.Models.Article;
+import com.baylor.se.project.bearnews.Service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ArticleController {
 
+    @Autowired
+    ArticleService articleService;
+
+    @RequestMapping(value = "/populateArticle", method = RequestMethod.POST)
+    public ResponseEntity<?> createTeam(@RequestBody Article articleSent){
+        articleService.createArticle(articleSent);
+        return new ResponseEntity(HttpStatus.OK);
+    }
 }
