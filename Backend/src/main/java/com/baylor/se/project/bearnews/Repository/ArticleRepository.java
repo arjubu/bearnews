71bc893fd450a1507cba983a29d3598e9827fc22
rahman-mush1311 <tuktuki1311@yahoo.com>
2022-11-17 15:25:42 -0600

    article repo created

--- Backend/src/main/java/com/baylor/se/project/bearnews/Repository/ArticleRepository.java
+++ Backend/src/main/java/com/baylor/se/project/bearnews/Repository/ArticleRepository.java
@@ -1,4 +1,13 @@
 package com.baylor.se.project.bearnews.Repository;
 
-public interface ArticleRepository {
+import com.baylor.se.project.bearnews.Models.Article;
+import com.baylor.se.project.bearnews.Models.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
+public interface ArticleRepository extends JpaRepository<Article,Long> {
 }
