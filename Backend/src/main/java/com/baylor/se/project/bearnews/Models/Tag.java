f0ce335aa720749257ad5e09b3c9cbf545e771df
Sadia Nasrin Tisha <sadiatisha@Sadias-MacBook-Air.local>
2022-11-17 17:01:01 -0600

    defined the tag model with id

--- Backend/src/main/java/com/baylor/se/project/bearnews/Models/Tag.java
+++ Backend/src/main/java/com/baylor/se/project/bearnews/Models/Tag.java
@@ -1,4 +1,19 @@
 package com.baylor.se.project.bearnews.Models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Tag {
+    @Id
+    @GeneratedValue
+    private long id;
 }
