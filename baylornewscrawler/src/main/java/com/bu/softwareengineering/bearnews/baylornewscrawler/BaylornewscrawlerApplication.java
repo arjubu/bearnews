ea6d15f1d443e1390b2800fbc8fbfefc0156ca5f
Ashfakur Rahman Arju <ashfak@Ashfakurs-MacBook-Pro.local>
2022-11-18 02:49:22 -0600

    baylor news crawler added

---
+++ baylornewscrawler/src/main/java/com/bu/softwareengineering/bearnews/baylornewscrawler/BaylornewscrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.bu.softwareengineering.bearnews.baylornewscrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaylornewscrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaylornewscrawlerApplication.class, args);
+	}
+
+}
