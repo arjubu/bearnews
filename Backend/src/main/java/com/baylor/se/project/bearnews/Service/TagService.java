9dfc751725c9bae73922261bab020a4b0871bfb7
Sadia Nasrin Tisha <sadiatisha@Sadias-MacBook-Air.local>
2022-11-17 18:28:40 -0600

    API Added: Search

--- Backend/src/main/java/com/baylor/se/project/bearnews/Service/TagService.java
+++ Backend/src/main/java/com/baylor/se/project/bearnews/Service/TagService.java
@@ -2,11 +2,13 @@ package com.baylor.se.project.bearnews.Service;
 
 import com.baylor.se.project.bearnews.Models.Tag;
 import com.baylor.se.project.bearnews.Repository.TagRepository;
-import com.baylor.se.project.bearnews.Repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TagService {
@@ -19,4 +21,15 @@ public class TagService {
     }
 
     public List<Tag> getAllTag() {return tagRepository.findAll();}
+
+    public Tag getTagById(Long id) {
+        Optional<Tag> tag = tagRepository.findById(id);
+
+        if (tag.isPresent()) {
+            return tag.get();
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No tag record exist for given id");
+        }
+    }
+
 }
