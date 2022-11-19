883a459a5f6218926b5dbd6cf830d9b55af690a2
Sadia Nasrin Tisha <sadiatisha@Sadias-MacBook-Air.local>
2022-11-18 18:32:40 -0600

    Event Added

---
+++ Backend/src/main/java/com/baylor/se/project/bearnews/Controller/EventController.java
@@ -0,0 +1,31 @@
+package com.baylor.se.project.bearnews.Controller;
+
+import com.baylor.se.project.bearnews.Models.Event;
+import com.baylor.se.project.bearnews.Service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class EventController {
+    @Autowired
+    EventService eventService;
+
+
+    @PostMapping("/event")
+    public ResponseEntity<Event> createEvent(@RequestBody Event event) {
+        Event updated = eventService.createEvent(event);
+        return new ResponseEntity<>(updated, new HttpHeaders(), HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/allevent", method = RequestMethod.GET)
+    public ResponseEntity<Event> getEvents() {
+        return new ResponseEntity(eventService.getAllEvent(), HttpStatus.OK);
+
+    }
+
+
+}
---
+++ Backend/src/main/java/com/baylor/se/project/bearnews/Models/Event.java
@@ -0,0 +1,49 @@
+package com.baylor.se.project.bearnews.Models;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String description;
+
+
+    @Column
+    private Date startdate;
+
+    @Column
+    private Date enddate;
+
+    @Column
+    private String location;
+
+
+//    @ManyToOne // owning-side
+//    @JoinColumn(name = "user_id")
+//    @JsonIdentityInfo(
+//            generator = ObjectIdGenerators.PropertyGenerator.class,
+//            property = "id")
+//    @JsonIdentityReference(alwaysAsId = true)
+//    private Users user;
+
+
+
+}
