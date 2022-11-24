819e2c03ebf80cd719632550d3da39ee54e11b52
Arju <Muhammad_Arju1@baylor.edu>
2022-11-23 23:54:11 -0600

    Baylor News Main Page Parsing Added in BarlorNewsCrawler

--- baylornewscrawler/src/main/java/com/bu/softwareengineering/bearnews/baylornewscrawler/controller/BaylorNewsController.java
+++ baylornewscrawler/src/main/java/com/bu/softwareengineering/bearnews/baylornewscrawler/controller/BaylorNewsController.java
@@ -25,9 +25,37 @@ public class BaylorNewsController {
 
     @GetMapping("/get-baylor-news")
     public ResponseEntity<?> getBaylorNews() throws IOException {
+        List<BaylorNews> newsItems = new ArrayList<>();
+
+
+        Document mainPageDocument = Jsoup.connect("https://www.baylor.edu/bn/").get();
+        Element uiNewsWidgetElement = mainPageDocument.getElementById("uiNewsWidget");
+
+        for(Element element : uiNewsWidgetElement.getElementsByTag("a")){
+            BaylorNews baylorNews = new BaylorNews();
+            String detailsHref = element.selectFirst("a").attr("href");
+            Long baylorNewsId = Long.valueOf(detailsHref.split("&")[1].split("=")[1]); //.split(":")[2]
+
+            String thumbnailLinkWithSpecialChar  = element.getElementsByClass("uiWidget-cards-item-image").first().attr("style");
+            String thumbnailLink = thumbnailLinkWithSpecialChar.substring(thumbnailLinkWithSpecialChar.indexOf("(") + 1, thumbnailLinkWithSpecialChar.indexOf(")"));
+            //String thumbnailLink = "http:"+ thumbnailLinkWithSpecialChar.substring(0, thumbnailLinkWithSpecialChar.length()-1);
+            String date = element.getElementsByClass("uiWidget-cards-item-date").first().text().replace(".","").replace(",","");
+            String title = element.getElementsByClass("uiWidget-cards-item-title").first().text();
+            String description = element.getElementsByClass("uiWidget-cards-item-description").first().text();
+
+            baylorNews.setBaylorNewsId(baylorNewsId);
+            baylorNews.setThumbnail(thumbnailLink);
+            baylorNews.setDate(date);
+            baylorNews.setTitle(title);
+            baylorNews.setDetailLink(detailsHref);
+            baylorNews.setDescription(description);
+
+            newsItems.add(baylorNews);
+        }
+
+
         Document document = Jsoup.connect("https://www.baylor.edu/bn/news.php").get();
         Elements elements = document.getElementsByClass("newsWidget");
-        List<BaylorNews> newsItems = new ArrayList<>();
         for(Element e : elements){
             for(int i=1; i<=e.childrenSize(); i++){
                 Element newsElement = e.getElementsByClass("newsWidget-item-"+i).get(0);
