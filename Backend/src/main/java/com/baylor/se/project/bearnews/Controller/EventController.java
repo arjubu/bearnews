69199b01a739f0f5163ce458519b9e046306ac13
Sadia Nasrin Tisha <sadiatisha@Sadias-MacBook-Air.local>
2022-11-28 20:38:18 -0600

    event_api_added

--- Backend/src/main/java/com/baylor/se/project/bearnews/Controller/EventController.java
+++ Backend/src/main/java/com/baylor/se/project/bearnews/Controller/EventController.java
@@ -3,12 +3,16 @@ package com.baylor.se.project.bearnews.Controller;
 
 
 import com.baylor.se.project.bearnews.Models.Event;
+import com.baylor.se.project.bearnews.ResponseObjectMappers.ArticleWithUsersObjectMapper;
 import com.baylor.se.project.bearnews.Service.EventService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EventController {
@@ -28,6 +32,14 @@ public class EventController {
         return new ResponseEntity(eventService.getAllEvent(), HttpStatus.OK);
 
     }
+
+    @RequestMapping(value = "/deleteEventById", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteAnEvent( @RequestParam (name="eventId" , required = true) Long eventId) throws JsonProcessingException {
+        String responseReturned = eventService.deleteAnEvent(eventId);
+        return new ResponseEntity(responseReturned,HttpStatus.OK);
+
+    }
+    
 
 
 
--- Backend/src/main/java/com/baylor/se/project/bearnews/Service/EventService.java
+++ Backend/src/main/java/com/baylor/se/project/bearnews/Service/EventService.java
@@ -1,6 +1,7 @@
 package com.baylor.se.project.bearnews.Service;
 
 
+import com.baylor.se.project.bearnews.Models.Article;
 import com.baylor.se.project.bearnews.Models.Event;
 
 import com.baylor.se.project.bearnews.Models.Event;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EventService {
@@ -23,4 +25,22 @@ public class EventService {
     }
 
     public List<Event> getAllEvent() {return eventRepository.findAll();}
+
+    public Event fetchEvenet(Long id) {
+        Optional<Event> eventQueryOpt = eventRepository.findById(id);
+        if (eventQueryOpt.isPresent())
+            return eventQueryOpt.get();
+        return null;
+    }
+
+    public String deleteAnEvent(Long id){
+        if(fetchEvenet(id)==null)
+            return "event id doesn't exsists";
+        else{
+            eventRepository.deleteById(id);
+            return "deleted successfully";
+        }
+    }
 }
+
+
