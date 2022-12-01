53cf4567ad09d313e07b7524e54220f85bcc691a
rahman-mush1311 <tuktuki1311@yahoo.com>
2022-11-30 21:12:30 -0600

    fetch all articles redundant list working now need tags

--- Backend/src/main/java/com/baylor/se/project/bearnews/Service/ArticleService.java
+++ Backend/src/main/java/com/baylor/se/project/bearnews/Service/ArticleService.java
@@ -210,33 +210,55 @@ public class ArticleService {
 
     }
     public List<ArticleWithUsersObjectMapper> getAllArticles(){
-        ServiceResponseHelper serviceResponseHelper = new ServiceResponseHelper(false,null,null);
-        Map errorResponse = new HashMap<>();
-        Map successResponse = new HashMap<>();
-
-        List<Users> systemUsersArticles = usersService.findingUsersCreatedArticles();
-        if(systemUsersArticles.isEmpty()){
+//        ServiceResponseHelper serviceResponseHelper = new ServiceResponseHelper(false,null,null);
+//        Map errorResponse = new HashMap<>();
+//        Map successResponse = new HashMap<>();
+//
+//        List<Users> systemUsersArticles = usersService.findingUsersCreatedArticles();
+//        if(systemUsersArticles.isEmpty()){
+//            return null;
+//        }
+//        List<ArticleWithUsersObjectMapper> articleDetails = new ArrayList<>();
+//        for(Users us: systemUsersArticles){
+//            for(Article a: us.getArticles()){
+//                ArticleWithUsersObjectMapper article = new ArticleWithUsersObjectMapper();
+//                article.setTitleOfArticle(a.getTitle());
+//                article.setContentOfArticle(a.getContent());
+//                article.setIdOfArticle(a.getId());
+//                article.setTimeOfArticleCretion(a.getCreatedAt());
+//
+//                article.setNameofCreator(us.getFirstName());
+//                article.setIdOfCreator(us.getId());
+//
+//                article.setIdOfTag(a.getContains().getId());
+//                article.setTextOfTag(a.getContains().getTagText());
+//
+//                articleDetails.add(article);
+//            }
+//        }
+//
+//        return articleDetails;
+        List<Article> articleList = articleRepository.findAll();
+        if(articleList.isEmpty()){
             return null;
         }
         List<ArticleWithUsersObjectMapper> articleDetails = new ArrayList<>();
-        for(Users us: systemUsersArticles){
-            for(Article a: us.getArticles()){
-                ArticleWithUsersObjectMapper article = new ArticleWithUsersObjectMapper();
+        for(Article a: articleList){
+            ArticleWithUsersObjectMapper article = new ArticleWithUsersObjectMapper();
                 article.setTitleOfArticle(a.getTitle());
                 article.setContentOfArticle(a.getContent());
                 article.setIdOfArticle(a.getId());
                 article.setTimeOfArticleCretion(a.getCreatedAt());
 
-                article.setNameofCreator(us.getFirstName());
-                article.setIdOfCreator(us.getId());
+                Users author= usersService.findingArticlesByUser(a.getId());
+                article.setIdOfCreator(author.getId());
+                article.setNameofCreator(author.getFirstName());
 
                 article.setIdOfTag(a.getContains().getId());
                 article.setTextOfTag(a.getContains().getTagText());
 
                 articleDetails.add(article);
-            }
         }
-
         return articleDetails;
 
     }
