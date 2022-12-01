6e5f884382aa0cfe0b4dcb27d8292ccb269f759b
rahman-mush1311 <tuktuki1311@yahoo.com>
2022-12-01 16:01:27 -0600

    search tags by letter api added

--- Backend/src/main/java/com/baylor/se/project/bearnews/Controller/TagController.java
+++ Backend/src/main/java/com/baylor/se/project/bearnews/Controller/TagController.java
@@ -4,11 +4,17 @@ package com.baylor.se.project.bearnews.Controller;
 import com.baylor.se.project.bearnews.Models.Tag;
 import com.baylor.se.project.bearnews.Repository.TagRepository;
 import com.baylor.se.project.bearnews.Service.TagService;
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
@@ -43,4 +49,22 @@ public class TagController {
         return new ResponseEntity<>(tag, new HttpHeaders(), HttpStatus.OK);
     }
 
+    @RequestMapping(value = "/getTagByLetter", method = RequestMethod.GET)
+    public ResponseEntity<?> getTagsByLetter(@RequestBody Map<String, String> sentLetter) throws JsonProcessingException {
+        String letterToSearch = sentLetter.get("suggString").toLowerCase();
+
+
+        ServiceResponseHelper serviceResponseHelper =  tagService.listOfTags(letterToSearch);;
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        if(serviceResponseHelper.getHasError()){
+            return new ResponseEntity<>(objectMapper.writeValueAsString(serviceResponseHelper),HttpStatus.BAD_REQUEST);
+        }
+        else {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("data", serviceResponseHelper.getContent());
+            return new ResponseEntity<>(map,HttpStatus.OK);
+        }
+
+    }
 }
