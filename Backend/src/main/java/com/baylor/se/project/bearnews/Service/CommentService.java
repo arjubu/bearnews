b643acee2ea11bd26310eda8db0c3cefb2b0b882
Sadia Nasrin Tisha <48999587+Sadia16101101@users.noreply.github.com>
2022-12-02 13:38:42 -0600

    Update CommentService.java

--- Backend/src/main/java/com/baylor/se/project/bearnews/Service/CommentService.java
+++ Backend/src/main/java/com/baylor/se/project/bearnews/Service/CommentService.java
@@ -116,9 +116,27 @@ public class CommentService {
         }
     }
 
-    public String deleteComment(Long id){
+public void deleteComment(Long id){
+
+        List<Article> articleList = articleRepository.findAll();
+        for(Article a: articleList){
+            for(Comment c1: a.getArticlecomments()){
+                if(c1.getId()==id){
+                    a.getArticlecomments().remove(c1);
+                    break;
+                }
+            }
+        }
+        List<Users> usersList = usersRepository.findAll();
+        for(Users u: usersList){
+            for(Comment c2: u.getComments()){
+                if(c2.getId()==id){
+                    u.getComments().remove(c2);
+                    break;
+                }
+            }
+        }
         commentRepository.deleteById(id);
-        return "deleted successfully";
     }
 
 
