41f454b92de9057e0bb55eae7f161ef71e00b7b6
miao tingshuo <miaotingshuo@gmail.com>
2022-12-03 01:15:46 -0600

    quick change with button

--- FrontEnd/src/components/post/post-format/elements/SocialShareBottom.jsx
+++ FrontEnd/src/components/post/post-format/elements/SocialShareBottom.jsx
@@ -1,30 +1,17 @@
 const SocialShareBottom = () => {
   return (
     <div className="post-shares m-t-xs-60">
-      <div className="title">SHARE:</div>
+      <div className="title">Operation:</div>
       <ul className="social-share social-share__rectangular">
-        <li>
-          <a href="#" className="btn bg-color-twitch">
-            <i className="fab fa-twitch" />
-            1K+
-          </a>
-        </li>
-        <li>
-          <a href="#" className="btn bg-color-facebook">
-            <i className="fab fa-facebook-f" />
-            1K+
-          </a>
-        </li>
         <li>
           <a href="#" className="btn bg-color-twitter">
             <i className="fab fa-twitter" />
-            1000+
+            
           </a>
         </li>
         <li>
           <a href="#" className="btn bg-color-linkedin">
             <i className="fab fa-linkedin-in" />
-            1M+
           </a>
         </li>
       </ul>
