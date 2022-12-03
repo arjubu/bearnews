d45b5eb413bd3c0a693135b3cb88212014e280e3
Sadia16101101 <sadia.tisha98@gmail.com>
2022-12-03 01:52:49 -0600

    addLikeFrontEnd

--- FrontEnd/src/components/post/post-format/elements/SocialShareBottom.jsx
+++ FrontEnd/src/components/post/post-format/elements/SocialShareBottom.jsx
@@ -1,13 +1,11 @@
+import Like from "./Like"
 const SocialShareBottom = () => {
   return (
     <div className="post-shares m-t-xs-60">
       <div className="title">Operation:</div>
       <ul className="social-share social-share__rectangular">
         <li>
-          <a href="#" className="btn bg-color-twitter">
-            <i className="fab fa-twitter" />
-            
-          </a>
+        <Like/>
         </li>
         <li>
           <a href="#" className="btn bg-color-linkedin">
