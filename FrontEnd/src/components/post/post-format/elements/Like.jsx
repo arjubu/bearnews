d29d074c8a61502d11bebb05c07b04df86d26bb8
miao tingshuo <miaotingshuo@gmail.com>
2022-12-04 01:05:19 -0600

    Like added

--- FrontEnd/src/components/post/post-format/elements/Like.jsx
+++ FrontEnd/src/components/post/post-format/elements/Like.jsx
@@ -6,16 +6,45 @@ const LikeButton = ({slug}) => {
   const [isClicked, setIsClicked] = useState(false);
 
   useEffect(() => {
-    
+    fetch('http://localhost:8080/likecount?articleId='+slug
+  )
+    .then(response => {
+       
+      if (response.status == 200) {
+        console.log('go'); 
+        return response.json();
+        
+      } else {
+        
+        console.log("Something went wrong ...");
+
+      }
+        
+      }).then(data=>{
+            setLikes(data);
+            return data;
+        //console.log(Mylist);
+      });
   });
 
   const handleClick = () => {
-    if (isClicked) {
-      setLikes(likes - 1);
-    } else {
-      setLikes(likes + 1);
-    }
-    setIsClicked(!isClicked);
+    window.location.reload(false);
+
+    fetch('http://localhost:8080/addlike?articleId='+slug
+    )
+      .then(response => {
+         
+        if (response.status == 200) {
+          console.log('go'); 
+         
+          
+        } else {
+          setLikes(likes+1);
+          console.log("Something went wrong ...");
+  
+        }
+          
+        });
   };
 
   return (
