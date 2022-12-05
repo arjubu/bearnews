251983271e044acfababef1bd5740280ebaddda7
rahman-mush1311 <tuktuki1311@yahoo.com>
2022-12-04 21:46:46 -0600

    cross origin changed

--- Backend/src/main/java/com/baylor/se/project/bearnews/Controller/EventController.java
+++ Backend/src/main/java/com/baylor/se/project/bearnews/Controller/EventController.java
@@ -24,7 +24,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/event")
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("*")
 public class EventController {
 
     @Autowired
